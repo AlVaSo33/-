1df4a851086769e127eb3b5556ddfe186f942fd7
Alexander Soloviev <asolovev@yandex.ru>
2022-10-13 23:08:44 +0300

    123

---
+++ Exampie_1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the number:");
+int number = int.Parse(Console.ReadLine()!);
+int n = number%10;
+Console.WriteLine("Остаток" + number%10);
